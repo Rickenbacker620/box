8f7785087be7479c30850e749ebe9c722d049da0
Rickenbacker620 <fu78sion@gmail.com>
2025-11-23 01:11:19 -0500

    1

--- box-frontend/src/config.ts
+++ box-frontend/src/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8787',
-  apiToken: import.meta.env.VITE_API_TOKEN || 'supersecret',
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
+  apiToken: import.meta.env.VITE_API_TOKEN
 } as const
